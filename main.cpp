6ba0c8645ee26b102328f0d3befbc382de624ede
MarcGilcher <107708453+MarcGilcher@users.noreply.github.com>
2022-07-04 16:53:55 +0000

    initial work on Project3/Part1

--- main.cpp
+++ main.cpp
@@ -53,51 +53,51 @@ Sub Object 1: Steering Wheel
     Name 4 things you'll find on the:    Steering Wheel
         1) paddle shifters
         2) 'cruise control' controls
-        3) 
-        4) 
+        3) 'phone answering' knob
+        4) volume controls
     Name 2 things you can do with the:   Steering Wheel
         1) adjust cruise control settings.
-        2)
+        2) adjust volume setting
         
 Sub Object 2: Instrument Cluster
     Name 4 things you'll find on the:   Instrument Cluster
-        1)
-        2)
-        3)
-        4)
+        1) speedometer
+        2) tachometer
+        3) fuel gauge
+        4) onboard computer display
     Name 3 things you can do with the:   Instrument Cluster
-        1)
-        2)
-        3)
+        1) observe car speed
+        2) observe motor rpm
+        3) observe tank capacity
     
 Sub Object 3: Environment Controls
     Name 3 things you'll find on the:    Environment Controls
-        1)
-        2)
-        3)
+        1) temperature control
+        2) fan speed control
+        3) automatic cooling control
     Name 3 things you can do with the:   Environment Controls
-        1)
-        2)
-        3)
+        1) set the temperature
+        2) set the fan speed
+        3) turn on automatic cooling
 
 Sub Object 4: Infotainment System
     Name 3 things you'll find on the:    Infotainment System
-        1)
-        2)
-        3)
+        1) radio
+        2) navigation
+        3) hand free telephone
     Name 3 things you can do with the:   Infotainment System
-        1)
-        2)
-        3)
+        1) set radio station
+        2) start navigation
+        3) start phone call
 
 Sub Object 5: Seat 
     Name 3 things you'll find on the:    Seat
-        1)
-        2)
-        3)
+        1) backrest
+        2) seating surface
+        3) head rest
     Name 2 things you can do with the:   Seat
-        1)
-        2)
+        1) adjust angle of backrest
+        2) adjust position of seating surface
 */
 
 /*
@@ -154,61 +154,7 @@ Part 1b Assignment: 1 Step
         'int float double bool char std::string'.
 */
 
-/*
-Thing 1)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
 
-/*
-Thing 2)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
-
-/*
-Thing 3)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
-
-/*
-Thing 4)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
 
  /*
 Part 1c - Lesson
@@ -279,19 +225,7 @@ Define an object that is made of 5 sub-objects.
     you'll define these 5 sub-objects in Part 1d.
 */
 
-/*
-Thing 10)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
+
 
 /*
 Part 1d - Lesson
@@ -384,75 +318,7 @@ each property for UDTs 5 - 9.
 Step 3 is written below UDTs 5 - 9
 */
 
-/*
-Thing 5)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
 
-/*
-Thing 6)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
-
-/*
-Thing 7)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
-
-/*
-Thing 8)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
-
-/*
-Thing 9)
-5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
-3 things it can do:
-    1)
-    2)
-    3)
- */
 
 /*
 ===================
@@ -472,8 +338,145 @@ After you have MOVED your 10 UDTs, send me a DM with your pull request link.
 I will review the pseudo-code that you have written.
 */
 
+/*
+Thing 1) Midi Keyboard
+5 properties:
+    1) number of keys (int)
+    2) number of usb ports (int)
+    3) number of control knobs (int)
+    4) input voltage level (double)
+    5) housing color (std::string)
+3 things it can do:
+    1) transmit output midi data
+    2) play arpeggio
+    3) transmit midi controller data
+ */
 
+/*
+Thing 2) Car
+5 properties:
+    1) number of wheels (int)
+    2) number of seats (int)
+    3) maximum velocity (double)
+    4) amount of average fuel (double)
+    5) maximum amount of load (double)
+3 things it can do:
+    1) drive forward
+    2) comsume fuel
+    3) transport passengers
+ */
 
+/*
+Thing 3) Bicycle
+5 properties:
+    1) diameter of wheels (double)
+    2) size of frame (double)
+    3) number of gears (int)
+    4) type of brakes (char())
+    5) price (double)
+3 things it can do:
+    1) transport a human
+    2) shift gear
+    3) decelarate
+ */
+
+/*
+Thing 4) Player
+5 properties:
+    1) name of the player (st::string)
+    2) start position x coordinate (float)
+    3) start position y coordinate (float)
+    4) speed in x direction (double)
+    5) time since last shoot (int)
+3 things it can do:
+    1) move in x direction
+    2) move in y direction
+    3) shoot bullet
+ */
+
+/*
+Thing 5) Woofer
+5 properties:
+    1) cone diameter in cm (int)
+    2) resonance frequency in Hertz (doulbe)
+    3) overall lenght in cm (int)
+    4) membrane area in cm² (double)
+    5) sound pressure level in dB/W/m (float)
+3 things it can do:
+    1) converts electrical energy into air vibration
+    2) moves the air in the tube
+    3) reproduces the low frequencies
+ */
+
+/*
+Thing 6) Tweeter
+5 properties:
+    1) dome diameter in cm (double)
+    2) voice coil diameter in cm (double)
+    3) resonance frequency in hz (double)
+    4) sound pressure level in dB/W/m (float)
+    5) dc resistance in ohm (double)
+3 things it can do:
+    1) produce the upper range of sound
+    2) convert electrical signal to acoustical waveforms
+    3) connects to an amplifier
+ */
+
+/*
+Thing 7) Enclosure
+5 properties:
+    1) lenght in cm (int)
+    2) width in cm (int)
+    3) height in cm (int)
+    4) volume in liter (float)
+    5) weight in grams (int)
+3 things it can do:
+    1) provide resonance volume
+    2) provide mounting for woofer
+    3) provide mounting for terminal
+ */
+
+/*
+Thing 8) Terminal
+5 properties:
+    1) number of connections (int)
+    2) number of fixation points (int)
+    3) lenght in cm (int)
+    4) width in cm (int)
+    5) number of switches (int)
+3 things it can do:
+    1) provide ports
+    2) provide switches
+    3) control volume
+ */
+
+/*
+Thing 9) Amplifier
+5 properties:
+    1) amount of power consumed in watt-hours (float)
+    2) number of inputs (int)
+    3) maximum output power in watt (double)
+    4) lenght in cm (int)
+    5) width in cm (int)
+3 things it can do:
+    1) amplify an electrical signal
+    2) consume electrical power
+    3) split the audio signal into frequency bands
+ */
+
+/*
+Thing 10) Active Loudspeaker Box
+5 properties:
+    1) Woofer
+    2) Tweeter
+    3) Enclosure
+    4) Terminal
+    5) Amplifier
+3 things it can do:
+    1) play audio signal
+    2) amplify audio signal
+    3) comsume electrical power
+ */
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
